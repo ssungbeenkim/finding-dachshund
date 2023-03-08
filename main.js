dd4cfa247fd282077dd247b746db9045d58c5fe5
wandakim <nwd1583@naver.com>
2023-03-08 16:14:42 +0900

    JS: Timer and Popup

--- main.js
+++ main.js
@@ -1,12 +1,91 @@
 'use strict';
+const itemSize = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 3;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
-const itemSize = 80;
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
 
-const startBtn = document.querySelector('.game__button');
-startBtn.addEventListener('click', initGame);
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopBtn();
+  showTimerAndScore();
+  startGameTimer(GAME_DURATION_SEC);
+}
+
+function stopGame() {
+  stopGameTimer();
+  hideGameButton();
+  showPopUpWithText('REPLAY❓');
+}
+
+function showStopBtn() {
+  const icon = gameBtn.querySelector('.fa-play');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
+}
+
+function hideGameButton() {
+  gameBtn.style.visibility = 'hidden';
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = 'visible';
+  gameScore.style.visibility = 'visible';
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    updateTimerText(remainingTimeSec);
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopUpWithText(text) {
+  popUpText.innerText = text;
+  popUp.classList.remove('pop-up--hide');
+}
 
 function initGame() {
+  field.innerHTML = '';
+  gameScore.innerHTML = CARROT_COUNT;
   addItem('carrot', 5, 'img/carrot.png');
   addItem('bug', 5, 'img/bug.png');
 }
