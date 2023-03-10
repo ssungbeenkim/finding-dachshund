c7390b0dc0df517bf07c77ded4d65a57f4ea7095
wandakim <nwd1583@naver.com>
2023-03-10 18:09:19 +0900

    Finished

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import * as sound from './sound.js';
 const gameFinishBanner = new PopUp();
 
 const game = new GameBuilder()
-  .withGameDuration(3)
-  .withCarrotCount(3)
-  .withBugCount(3)
+  .withGameDuration(15)
+  .withCarrotCount(20)
+  .withBugCount(20)
   .build();
 
 game.setGameStopListener((reason) => {
