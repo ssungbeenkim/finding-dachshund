671319a054dee34153b97450b2e98cfc9df4779c
wandakim <nwd1583@naver.com>
2023-04-05 11:04:58 +0900

    Sound re-design

--- src/sound.js
+++ src/sound.js
@@ -1,4 +1,4 @@
-const alertSound = new Audio('./sound/alert.wav');
+const alertSound = new Audio('./sound/alert.mp3');
 const bgSound = new Audio('./sound/bg.mp3');
 const bugSound = new Audio('./sound/bug_pull.mp3');
 const winSound = new Audio('./sound/game_win.mp3');
