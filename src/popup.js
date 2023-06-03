0bf1c09dc1ba5e3f0568d7f45f892ae8ead70ebb
wandakim <nwd1583@naver.com>
2023-06-03 19:04:04 +0900

    Edit Base URL

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const BASE_URL = 'https://find-dachshund.herokuapp.com';
+const BASE_URL = 'https://find-dachshund.herokuapp.com/scores';
 
 export default class PopUp {
   constructor() {
